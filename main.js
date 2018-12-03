79248277cc2072f1f883f577117456c21fb64a67
Subhan Nizar <subhannizarcha@gmail.com>
2018-12-03 14:13:32 +0500

    Chat

--- main.js
+++ main.js
@@ -1,227 +1,114 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var mongo = require('mongoose');
-const bodyParser = require('body-parser');
+var axios = require('axios');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var moment = require('moment');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-//users count
-var users = 0;
-//index route
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/register.html");
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+var users = {};
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "findnspot"
 });
-//index route
-app.get('/chat/:id', function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
 });
-//friends route
-app.get('/friends', function(req, res) {
-    res.sendFile(__dirname + "/friends.html");
-});
-//mongo connection
-mongo.connect('mongodb://localhost/chat', function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Mongoose ready');
-    }
-});
-//Creating schema 
-var chatSchema = mongo.Schema({
-    msg: String,
-    to: String,
-    from: String,
-    from_name: String,
-    time: { type: Date, default: Date.now }
-});
-//Creating collection in db
-var Chat = mongo.model('Message', chatSchema);
 //connection established
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
+//new chat msg
+    socket.on('chatadd', function (obj) {
 
-    function getOldMsgs(to, from, room) {
+        var sql = "INSERT INTO chats (sender, receiver, message, room_id, created_at ) " +
+            "VALUES ('"+obj.sender+"','"+obj.receiver+"','"+sanitizeForMySQL(obj.msg)+"','"+obj.room_id+"','"+moment.utc(new Date()).format('YYYY-MM-DD HH:mm:ss')+"')";
 
-        //retrieve msg
-        var query = Chat.find({
-            $or: [
-                { $and: [{ to: to }, { from: from }] },
-                { $and: [{ to: from }, { from: to }] }
-            ]
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
         });
-        query.sort('time').exec(function(err, docs) {
-            if (err) {
-                throw err;
-            } else {
-                //send old msg to client
-                console.log('Sending to room:', room);
-                io.sockets.in(room).emit('oldMsgs', docs);
-            }
-        });
-    }
-
-    //new chat msg
-    socket.on('chatadd', function(msg) {
-
-        //get user data
-        UserReg.findOne({ ID: msg.from }, function(err, user) {
-            if (err) {
-                throw err;
-            } else {
-                //send old msg to client
-                var newMsg = new Chat({
-                    msg: msg.msg,
-                    to: msg.to,
-                    from: msg.from,
-                    from_name: user.Name
-                });
-
-
-                //saving msg in db
-                newMsg.save(function(err, docs) {
-                    if (err) {
-                        console.log(err);
-                        throw err;
-                    } else {
-                        //send old msg to client
-                        if (parseInt(msg.to) > parseInt(msg.from)) {
-                            room = msg.to + '-' + msg.from;
-                        } else {
-                            room = msg.from + '-' + msg.to;
-                        }
-                        getOldMsgs(msg.to, msg.from, room);
-                    }
-                });
-            }
-        });
+    console.log('Sent msg on room: ',obj.room_id);
+    io.sockets.in(obj.room_id).emit('updateChat', obj);
 
     });
     //new user added
-    socket.on('user', function(to, from) {
-
-
-        if (parseInt(to) > parseInt(from)) {
-            room = to + '-' + from;
-        } else {
-            room = from + '-' + to;
-        }
+    socket.on('userAddToRoom', function (roomID , user_id) {
 
         //subscribe to room
-        socket.user = from;
-        socket.join(room);
+        socket.user = user_id;
+        socket.join(roomID);
 
-
-        console.log('joining room', room);
-
-        data = {
-            "users": roomCount(room),
-            "room": room
-        };
-        getOldMsgs(to, from, room);
-        //send user count
-        io.emit('userUpdate', data);
+        console.log('joining room', roomID);
     });
-    //user inactive
-    socket.on('bye', function() {
-        if (users != 0 && users > 1) {
-            //remove user
-            users = users - 1;
-            //send updated user count
-            io.emit('userUpdate', users);
-        }
-        console.log('disconnected');
+
+    socket.on('postMsgSent',function (room_id , msgObj) {
+        io.sockets.in(room_id).emit('updateChat', msgObj);
+    });
+
+    socket.on('userActive' , function (id) {
+        users[socket.id] = id;
+        //subscribe to room
+        socket.user = id;
+        socket.join(id);
+        console.log('Active user id',users[socket.id]);
+    });
+
+
+    socket.on('disconnect', function () {
+        console.log('user ' + users[socket.id] + ' disconnected');
+        delete users[socket.id];
+    });
+
+    socket.on('checkActive',function (userID) {
+        return Object.values(users).indexOf(userID);
     });
 
 });
 
-
-io.on('disconnect', function() {
-    if (users != 0 && users > 1) {
-        //remove user
-        users = users - 1;
-        //send updated user count
-        io.emit('userUpdate', users);
-    }
-    console.log('disconnected');
-});
 //port 
-http.listen(3000, function() {
+http.listen(3000, function () {
     console.log('listening');
 });
-//adding user
 
-// Schema
-var RegSchema = mongo.Schema({
-    Name: String,
-    reg_time: {
-        type: Date,
-        default: Date.now
-    },
-    ID: String
-});
 
-// Model
-var UserReg = mongo.model('UserReg', RegSchema);
-app.post("/register", function(req, res) {
-    var data = { "msg": '' };
-
-    var checkExists = UserReg.findOne({ Name: req.body.Name }, function(err, user) {
-        if (err) {
-            console.log(err);
+function sanitizeForMySQL (str) {
+    return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
+        switch (char) {
+            case "\0":
+                return "\\0";
+            case "\x08":
+                return "\\b";
+            case "\x09":
+                return "\\t";
+            case "\x1a":
+                return "\\z";
+            case "\n":
+                return "\\n";
+            case "\r":
+                return "\\r";
+            case "\"":
+            case "'":
+            case "\\":
+            case "%":
+                return "\\"+char; // prepends a backslash to backslash, percent,
+                                  // and double/single quotes
         }
-        if (user) {
-            data.user = user.ID;
-        } else {
-
-            // Add in collection
-            var UserAdd = new UserReg({
-                Name: req.body.Name,
-                ID: generateID(10)
-            });
-
-            // Save
-            UserAdd.save(function(err, fluffy) {
-                if (err) return console.error(err);
-            });
-
-            data.user = UserAdd.ID;
-        }
-        res.send(data);
     });
-
-});
-
-function generateID(length) {
-    let text = ""
-    const possible = "0123456789"
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-
-    return text
 }
 
-//list people
-app.get('/people/:user_id', function name(req, res) {
-
-    //console.log(req);
-    //retrieve msg
-    var query = UserReg.find({ ID: { $ne: parseInt(req.params.user_id) } });
-    query.sort('-reg_time').exec(function(err, docs) {
-        if (err) {
-            throw err;
-        } else {
-            //send old msg to client
-            res.send(docs);
-        }
-    });
-});
-
-function roomCount(roomName) {
-    var roomCount = io.nsps['/'].adapter.rooms[roomName];
-    if (!roomCount) return null;
-    return roomCount.length;
+function inArray(needle, haystack) {
+    var length = haystack.length;
+    for(var i = 0; i < length; i++) {
+        if(haystack[i] == needle)
+            return true;
+    }
+    return false;
 }
